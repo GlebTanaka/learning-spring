cce041d196a3c6aa128ad8506506ded225ca4af0
GlebTanaka <69252964+GlebTanaka@users.noreply.github.com>
2023-11-04 13:32:20 +0100

    create spring project

---
+++ src/test/java/com/glebtanaka/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glebtanaka.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
